3d0e50d7eba27931c9fd9675d8d8c8c68deec16f
Shiv-30 <sosmart08642@gmail.com>
2020-11-16 17:44:58 +0530

    add-to-cart

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,40 @@
-console.log("helo");
+import axios from 'axios';
+import Noty from 'noty';
+
+let addToCart = document.querySelectorAll(".add-to-cart");
+let cartCounter = document.querySelector("#cartCounter");
+
+function updateToCart(pizza) {
+    axios.post('/update-cart', pizza).then( (res) => {
+        cartCounter.innerText = res.data.totalQty;
+        new Noty ({
+            type: 'success',
+            timeout: 1000,
+            text: 'Item added to cart',
+            progressBar: false,
+            layout: "topRight"
+
+        }).show();
+    }).catch(err => {
+        new Noty ({
+            type: 'error',
+            timeout: 1000,
+            text: 'Something went wrong',
+            progressBar: false,
+            layout: "topRight"
+
+        }).show();
+    });
+}
+
+
+
+addToCart.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        
+        let pizza = JSON.parse(btn.dataset.pizza);
+        updateToCart(pizza);
+        console.log(pizza);
+
+    });
+});
